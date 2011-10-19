61884cad7a36babf976f2a72e0fcb2009bce603f
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-10-19 15:47:53 +1100

    Added more os-specific stuff.

--- serial/open_darwin.go
+++ serial/open_darwin.go
@@ -12,19 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This file contains OS-specific constants and types.
+
 package serial
 
 import "io"
 import "os"
 
-// OS-specific constants.
+// termios types
+type cc_t byte
+type speed_t uint64
+type tcflag_t uint64
+
+// sys/termios.h
 const (
-	// sys/termios.h
 	B9600 = 9600
 	B14400 = 14400
 	B19200 = 19200
 
-	// sys/termios.h
 	CS5 = 0x00000000
 	CS6 = 0x00000100
 	CS7 = 0x00000200
@@ -32,11 +37,22 @@ const (
 	CLOCAL = 0x00008000
 	CREAD = 0x00000800
 	IGNPAR = 0x00000004
-	VMIN = Tcflag_t (16);
-	VTIME = Tcflag_t (17);
 
 	NCCS = 20;
+
+	VMIN = tcflag_t(16);
+	VTIME = tcflag_t(17);
 )
+
+type termios struct {
+	c_iflag tcflag_t
+	c_oflag tcflag_t
+	c_cflag tcflag_t
+	c_lflag tcflag_t
+	c_cc [NCCS]cc_t
+	c_ispeed speed_t
+	c_ospeed speed_t
+}
 
 func openInternal(options OpenOptions) (io.ReadWriteCloser, os.Error) {
 	return nil, os.NewError("Not implemented.")
