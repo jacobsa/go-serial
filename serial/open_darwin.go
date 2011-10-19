8c2fdc48e4d44e277c511c657473117b3892f543
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-10-19 15:46:31 +1100

    Added some constants.

--- serial/open_darwin.go
+++ serial/open_darwin.go
@@ -17,6 +17,27 @@ package serial
 import "io"
 import "os"
 
+// OS-specific constants.
+const (
+	// sys/termios.h
+	B9600 = 9600
+	B14400 = 14400
+	B19200 = 19200
+
+	// sys/termios.h
+	CS5 = 0x00000000
+	CS6 = 0x00000100
+	CS7 = 0x00000200
+	CS8 = 0x00000300
+	CLOCAL = 0x00008000
+	CREAD = 0x00000800
+	IGNPAR = 0x00000004
+	VMIN = Tcflag_t (16);
+	VTIME = Tcflag_t (17);
+
+	NCCS = 20;
+)
+
 func openInternal(options OpenOptions) (io.ReadWriteCloser, os.Error) {
 	return nil, os.NewError("Not implemented.")
 }
