20807638310f5065864a3d8f29d07fac36b76d0d
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-10-19 15:13:26 +1100

    Started on the OpenOptions struct.

---
+++ serial/serial.go
@@ -0,0 +1,23 @@
+// Copyright 2011 Aaron Jacobs. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package serial provides routines for interacting with serial ports.
+// Currently it supports only OS X; see the readme file for details.
+
+// OpenOptions is the struct containing all of the options necessary for
+// opening a serial port.
+type OpenOptions struct {
+	// The name of the port, e.g. "/dev/tty.usbserial-A8008HlV".
+	PortName string
+}
