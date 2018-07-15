d7c27e81ce2b6618238b692539285ac2a7a9a51e
Philip Z <philip_chang@163.com>
2018-07-16 00:51:03 +0800

    Update open_linux.go

--- serial/open_linux.go
+++ serial/open_linux.go
@@ -27,7 +27,7 @@ import (
 const (
 	kTCSETS2 = unix.TCSETS2 //0x8030542B
 	kBOTHER  = unix.BOTHER//0x00001000
-	kNCCS    = unix.NCCS //fix for openwrt
+	kNCCS    = 23 // unix.NCCS //fix for openwrt
 )
 /*
 TCSETS2 = 0x8030542B
