a3d778e6a0b2b6b5ff090218043cec8e14060adb
Andrew H <ahollister@plantiga.com>
2020-01-22 11:55:01 -0800

    define SerialPort type

---
+++ open_linux.go
@@ -0,0 +1,150 @@
+package serial
+
+import (
+	"errors"
+	"os"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+// Serial port type
+type SerialPort os.File
+
+const (
+	NCCS = 19
+)
+
+//
+// Types from asm-generic/termbits.h
+//
+
+type cc_t byte
+type speed_t uint32
+type tcflag_t uint32
+type termios2 struct {
+	c_iflag  tcflag_t   // input mode flags
+	c_oflag  tcflag_t   // output mode flags
+	c_cflag  tcflag_t   // control mode flags
+	c_lflag  tcflag_t   // local mode flags
+	c_line   cc_t       // line discipline
+	c_cc     [NCCS]cc_t // control characters
+	c_ispeed speed_t    // input speed
+	c_ospeed speed_t    // output speed
+}
+
+//
+// Returns a pointer to an instantiates termios2 struct, based on the given
+// OpenOptions. Termios2 is a Linux extension which allows arbitrary baud rates
+// to be specified.
+//
+func makeTermios2(options OpenOptions) (*termios2, error) {
+
+	// Sanity check inter-character timeout and minimum read size options.
+
+	vtime := uint(round(float64(options.InterCharacterTimeout)/100.0) * 100)
+	vmin := options.MinimumReadSize
+
+	if vmin == 0 && vtime < 100 {
+		return nil, errors.New("invalid values for InterCharacterTimeout and MinimumReadSize")
+	}
+
+	if vtime > 25500 {
+		return nil, errors.New("invalid value for InterCharacterTimeout")
+	}
+
+	ccOpts := [NCCS]cc_t{}
+	ccOpts[unix.VTIME] = cc_t(vtime / 100)
+	ccOpts[unix.VMIN] = cc_t(vmin)
+
+	t2 := &termios2{
+		c_cflag:  unix.CLOCAL | unix.CREAD | unix.BOTHER,
+		c_ispeed: speed_t(options.BaudRate),
+		c_ospeed: speed_t(options.BaudRate),
+		c_cc:     ccOpts,
+	}
+
+	switch options.StopBits {
+	case 1:
+	case 2:
+		t2.c_cflag |= unix.CSTOPB
+
+	default:
+		return nil, errors.New("invalid setting for StopBits")
+	}
+
+	switch options.ParityMode {
+	case Parity_None:
+	case Parity_Odd:
+		t2.c_cflag |= unix.PARENB
+		t2.c_cflag |= unix.PARODD
+
+	case Parity_Even:
+		t2.c_cflag |= unix.PARENB
+
+	default:
+		return nil, errors.New("invalid setting for ParityMode")
+	}
+
+	switch options.DataBits {
+	case 5:
+		t2.c_cflag |= unix.CS5
+	case 6:
+		t2.c_cflag |= unix.CS6
+	case 7:
+		t2.c_cflag |= unix.CS7
+	case 8:
+		t2.c_cflag |= unix.CS8
+	default:
+		return nil, errors.New("invalid setting for DataBits")
+	}
+
+	return t2, nil
+}
+
+func openInternal(options OpenOptions) (*SerialPort, error) {
+	file, openErr :=
+		os.OpenFile(
+			options.PortName,
+			unix.O_RDWR|unix.O_NOCTTY|unix.O_NONBLOCK,
+			0600)
+	if openErr != nil {
+		return nil, openErr
+	}
+
+	// Clear the non-blocking flag set above.
+	nonblockErr := unix.SetNonblock(int(file.Fd()), false)
+	if nonblockErr != nil {
+		return nil, nonblockErr
+	}
+
+	t2, optErr := makeTermios2(options)
+	if optErr != nil {
+		return nil, optErr
+	}
+
+	r, _, errno := unix.Syscall(
+		unix.SYS_IOCTL,
+		uintptr(file.Fd()),
+		uintptr(unix.TCSETS2),
+		uintptr(unsafe.Pointer(t2)))
+
+	if errno != 0 {
+		return nil, os.NewSyscallError("SYS_IOCTL", errno)
+	}
+
+	if r != 0 {
+		return nil, errors.New("unknown error from SYS_IOCTL")
+	}
+
+	return file, nil
+}
+
+func inwaitingInternal() (int, error) {
+	return 1, nil
+}
+
+func test() int {
+	var test SerialPort
+	test.Read()
+}
---
+++ serial.go
@@ -0,0 +1,104 @@
+// Copyright 2011 Aaron Jacobs. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package serial provides routines for interacting with serial ports.
+// Currently it supports only OS X; see the readme file for details.
+
+package serial
+
+import "math"
+
+// Valid parity values.
+type ParityMode int
+
+const (
+	Parity_None ParityMode = 0
+	Parity_Odd  ParityMode = 1
+	Parity_Even ParityMode = 2
+)
+
+// OpenOptions is the struct containing all of the options necessary for
+// opening a serial port.
+type OpenOptions struct {
+	// The name of the port, e.g. "/dev/tty.usbserial-A8008HlV".
+	PortName string
+
+	// The baud rate for the port.
+	BaudRate uint
+
+	// The number of data bits per frame. Legal values are 5, 6, 7, and 8.
+	DataBits uint
+
+	// The number of stop bits per frame. Legal values are 1 and 2.
+	StopBits uint
+
+	// The type of parity bits to use for the connection. Currently parity errors
+	// are simply ignored; that is, bytes are delivered to the user no matter
+	// whether they were received with a parity error or not.
+	ParityMode ParityMode
+
+	// Enable RTS/CTS (hardware) flow control.
+	RTSCTSFlowControl bool
+
+	// An inter-character timeout value, in milliseconds, and a minimum number of
+	// bytes to block for on each read. A call to Read() that otherwise may block
+	// waiting for more data will return immediately if the specified amount of
+	// time elapses between successive bytes received from the device or if the
+	// minimum number of bytes has been exceeded.
+	//
+	// Note that the inter-character timeout value may be rounded to the nearest
+	// 100 ms on some systems, and that behavior is undefined if calls to Read
+	// supply a buffer whose length is less than the minimum read size.
+	//
+	// Behaviors for various settings for these values are described below. For
+	// more information, see the discussion of VMIN and VTIME here:
+	//
+	//     http://www.unixwiz.net/techtips/termios-vmin-vtime.html
+
+	// For windows usage, these options (termios) do not conform well to the
+	//     windows serial port / comms abstractions.  Please see the code in
+	//		 open_windows setCommTimeouts function for full documentation.
+	//   	 Summary:
+	//			Setting MinimumReadSize > 0 will cause the serialPort to block until
+	//			until data is available on the port.
+	//			Setting IntercharacterTimeout > 0 and MinimumReadSize == 0 will cause
+	//			the port to either wait until IntercharacterTimeout wait time is
+	//			exceeded OR there is character data to return from the port.
+
+	InterCharacterTimeout uint
+	MinimumReadSize       uint
+}
+
+// Open creates an io.ReadWriteCloser based on the supplied options struct.
+func Open(options OpenOptions) (*SerialPort, error) {
+	// Redirect to the OS-specific function.
+	return openInternal(options)
+}
+
+func (*SerialPort) InWaiting() (int, error) {
+	return inWaitingInternal()
+}
+
+func test() int {
+	var test *SerialPort
+	testyy := make([]byte, 5)
+	test.Read(testyy)
+	test.InWaiting()
+	return 0
+}
+
+// Rounds a float to the nearest integer.
+func round(f float64) float64 {
+	return math.Floor(f + 0.5)
+}
