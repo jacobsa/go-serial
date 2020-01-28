2182e46ff6156438ffcaa2139d6e26e32eb9c796
Andrew Hollister <ahollister@plantiga.com>
2020-01-28 11:30:27 -0800

    add DTR and SetDTR methods to port

--- serial/port_linux.go
+++ serial/port_linux.go
@@ -44,6 +44,35 @@ func (p *Port) SetTimeout(t time.Time) error {
 	return nil
 }
 
+// Get the port's DTR pin state
+func (p *Port) DTR() (bool, error) {
+	var status int
+	_, _, err := unix.Syscall(unix.SYS_IOCTL, p.f.Fd(), unix.TIOCMGET, uintptr(unsafe.Pointer(&status)))
+	if err != 0 {
+		return false, err
+	}
+	if status&unix.TIOCM_DTR > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+// Set the port's DTR pin state
+func (p *Port) SetDTR(state bool) error {
+	// todo(ahollist): Implement
+	var command int
+	if state {
+		command = unix.TIOCMBIS
+	} else {
+		command = unix.TIOCMBIC
+	}
+	_, _, err := unix.Syscall(unix.SYS_IOCTL, p.f.Fd(), uintptr(command), 0)
+	if err != 0 {
+		return err
+	}
+	return nil
+}
+
 func NewPort(f *os.File) *Port {
 	return &Port{f}
 }
